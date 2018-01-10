76c401af124a4997b047d78769d3f44fb90ca5f3
cfncluster user <ec2-user@ip-172-31-26-245.ec2.internal>
2018-01-10 00:03:47 -0600

    Make tmp file number flexible along with file size.

--- genome_mapping/job_scripts/pre_1b.bam2fastq.sh
+++ genome_mapping/job_scripts/pre_1b.bam2fastq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -pe threaded 18
+#$ -pe threaded 4
 
 set -eu -o pipefail
 
@@ -16,8 +16,19 @@ FNAME=$2
 
 printf -- "---\n[$(date)] Start bam2fastq: $FNAME\n---\n"
 
-$SAMTOOLS collate -uOn 128 $SM/downloads/$FNAME $SM/tmp.collate \
-    |$SAMTOOLS fastq -O -F 0x900 -1 $SM/fastq/$SM.R1.fastq.gz -2 $SM/fastq/$SM.R2.fastq.gz -
-rm $SM/downloads/$FNAME
+FSIZE=$(stat -c%s $SM/downloads/$FNAME)
+
+if [[ $(($FSIZE/1024**3)) -lt 128 ]]; then
+    TMP_N=128
+elif [[ $(($FSIZE/1024**3)) -lt 1000 ]]; then
+    TMP_N=$(($FSIZE/1024**3+1))
+else
+    echo "The bam file is bigger than 1TB."
+    exit 1
+fi
+    
+$SAMTOOLS collate -uOn $TMP_N $SM/downloads/$FNAME $SM/tmp.collate \
+    |$SAMTOOLS fastq -F 0x900 -1 $SM/fastq/$SM.R1.fastq.gz -2 $SM/fastq/$SM.R2.fastq.gz -
+#rm $SM/downloads/$FNAME
 
 printf -- "---\n[$(date)] Finish bam2fastq: $FNAME\n"
