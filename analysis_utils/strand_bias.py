b2054695d110265141b718f0fa4d989b926ae722
cfncluster user <ec2-user@ip-172-31-1-20.ec2.internal>
2018-02-21 11:03:31 -0600

    Refactoring.

--- analysis_utils/strand_bias.py
+++ analysis_utils/strand_bias.py
@@ -4,10 +4,11 @@ import argparse
 import re
 import subprocess
 import sys
-import os
 import math 
 from rpy2.robjects import r
 from scipy.stats import fisher_exact
+from library.utils import coroutine, printer
+from library.pileup import pileup, clean, count
 
 def run(args):
     s_info = strand_info(pileup(args.bam, args.min_MQ, args.min_BQ, clean(count())))
@@ -25,13 +26,6 @@ def run(args):
         printer('{chrom}\t{pos}\t{ref}\t{alt}\t{strand_info}'.format(
             chrom=chrom, pos=pos, ref=ref.upper(), alt=alt.upper(), 
             strand_info=s_info.send((chrom, pos, ref, alt))))
-
-def coroutine(func):
-    def start(*args, **kwargs):
-        g = func(*args, **kwargs)
-        g.__next__()
-        return g
-    return start
 
 @coroutine
 def strand_info(target):
@@ -71,70 +65,6 @@ def strand_info(target):
             ref_n=ref_n, ref_fwd=ref_fwd, ref_rev=ref_rev, ref_ratio=ref_ratio,
             alt_n=alt_n, alt_fwd=alt_fwd, alt_rev=alt_rev, alt_ratio=alt_ratio, 
             p_fisher=p_fisher(ref_fwd, alt_fwd, ref_rev, alt_rev))
-
-@coroutine
-def pileup(bam, min_MQ, min_BQ, target):
-    result = None
-    while True:
-        chrom, pos = (yield result)
-        cmd = ['samtools', 'mpileup', '-d', '8000',
-               '-q', str(min_MQ), '-Q', str(min_BQ),
-               '-r', '{}:{}-{}'.format(chrom, pos, pos), bam]
-        cmd_out = subprocess.run(
-            cmd, universal_newlines=True,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        try:
-            cmd_out.check_returncode()
-        except subprocess.CalledProcessError:
-            sys.exit(cmd_out.stderr)
-        try:
-            bases = cmd_out.stdout.split()[4]
-        except IndexError:
-            bases = ''
-        result = target.send(bases)
-
-@coroutine
-def clean(target):
-    result = None
-    while True:
-        bases = (yield result)
-        bases = re.sub('\^.', '', bases)
-        bases = re.sub('\$', '', bases)
-        for n in set(re.findall('-(\d+)', bases)):
-            bases = re.sub('-{0}[ACGTNacgtn]{{{0}}}'.format(n), '', bases)
-        for n in set(re.findall('\+(\d+)', bases)):
-            bases = re.sub('\+{0}[ACGTNacgtn]{{{0}}}'.format(n), '', bases)
-        result = target.send(bases)
-
-@coroutine
-def count():
-    result = None
-    while True:
-        bases = (yield result)
-        base_n = {}
-        base_n['A'] = bases.count('A') 
-        base_n['C'] = bases.count('C')
-        base_n['G'] = bases.count('G')
-        base_n['T'] = bases.count('T')
-        base_n['a'] = bases.count('a')
-        base_n['c'] = bases.count('c')
-        base_n['g'] = bases.count('g')
-        base_n['t'] = bases.count('t')
-        base_n['dels'] = bases.count('*')
-        result = base_n
-
-def printer(out):
-    try:
-        print(out, flush=True)
-    except BrokenPipeError:
-        try:
-            sys.stdout.close()
-        except BrokenPipeError:
-            pass
-        try:
-            sys.stderr.close()
-        except BrokenPipeError:
-            pass
 
 def p_poisson(n_fwd, n_rev):
     return r("poisson.test(c({},{}))$p.value".format(n_fwd, n_rev))[0]
