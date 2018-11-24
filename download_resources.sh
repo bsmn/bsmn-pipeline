d16bb1685673973eb0c463cd59e74cc58cc5b483
Taejeong Bae <bintriz@gmail.com>
2018-11-24 10:41:53 -0600

    Add a script for resource download

---
+++ download_resources.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir -p resources
+
+# Download and index the human ref genome
+tools/python/3.6.2/bin/synapse get syn10347383 --downloadLocation resources/
+gunzip resources/hs37d5.fa.gz
+tools/bwa/0.7.16a/bin/bwa index resources/hs37d5.fa
+tools/samtools/1.7/bin/samtools faidx resources/hs37d5.fa
+tools/java/jdk1.8.0_191/bin/java -jar tools/picard/2.12.1/picard.jar \
+    CreateSequenceDictionary R=resources/hs37d5.fa O=resources/hs37d5.dict
+
+# Download mapping resources
+tools/python/3.6.2/bin/synapse get syn17062535 -r --downloadLocation resources/
+gunzip resources/*vcf.gz resources/*vcf.idx.gz
+rm resources/SYNAPSE_METADATA_MANIFEST.tsv
+
+# Split the ref genome by chromosome
+awk '{ 
+    r = match($1, "^>"); 
+    if (r != 0) {
+        filename = "resources/chr"substr($1, 2, length($1))".fa"; 
+        print $0 > filename;
+    } 
+    else {
+        print $0 >> filename;
+    }
+}' resources/hs37d5.fa
+rm resources/chrGL* resources/chrhs37d5.fa resources/chrNC_007605.fa
