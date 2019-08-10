fcfa65a93e45a478529d8b5af14536ae902a0b5a
Taejeong Bae <bintriz@gmail.com>
2019-08-09 21:15:40 -0500

    Modify to skip installing installed tools

--- install_tools.sh
+++ install_tools.sh
@@ -1,231 +1,361 @@
 #!/bin/bash
 
+set -o errexit -o nounset -o pipefail -o xtrace
+
 WD=$(pwd)
 
 # Installing Python3
-mkdir -p tools/python/3.6.2/src
-cd tools/python/3.6.2/src
-wget -qO- https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz \
-    |tar xvz --strip-components=1
-./configure --with-ensurepip=install --prefix=$WD/tools/python/3.6.2
-make
-make install
-cd $WD
-rm -r tools/python/3.6.2/src
+DIR=$WD/tools/python/3.6.2
+URL="https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvz --strip-components=1
+    ./configure --with-ensurepip=install --prefix=$DIR 
+    make
+    make install
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
 
 # Installing Python modules needed
-tools/python/3.6.2/bin/pip3 install --upgrade pip
-tools/python/3.6.2/bin/pip install awscli synapseclient statsmodels scipy rpy2
+$DIR/bin/pip3 install -Uq pip
+$DIR/bin/pip install -Uq awscli synapseclient statsmodels scipy rpy2 # for bsmn_pipeline itself
+$DIR/bin/pip install -Uq pysam numpy pandas pysamstats regex scipy pyfaidx # for MosaicForecast
+$DIR/bin/pip install -Uq cutadapt # for RetroSom
+
+unset DIR URL
 
 # Installling Java8
-mkdir -p tools/java
-cd tools/java
-wget -qO- --no-check-certificate --header "Cookie: oraclelicense=a" \
-    http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz \
-    |tar xvz
-cd $WD
+DIR=$WD/tools/java/jdk1.8.0_222
+URL="https://github.com/ojdkbuild/contrib_jdk8u-ci/releases/download/jdk8u222-b10/jdk-8u222-ojdkbuild-linux-x64.zip"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget $URL
+    unzip jdk*.zip
+    rm jdk*.zip
+    mv jdk*/* $DIR
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing bwa
-mkdir -p tools/bwa/0.7.16a/bin
-mkdir -p tools/bwa/0.7.16a/share/man/man1
-mkdir -p tools/bwa/0.7.16a/src
-cd tools/bwa/0.7.16a/src
-wget -qO- https://github.com/lh3/bwa/releases/download/v0.7.16/bwa-0.7.16a.tar.bz2 \
-    |tar xvj --strip-components=1
-make
-mv bwa ../bin
-mv *.pl ../bin
-mv bwa.1 ../share/man/man1
-cd $WD
-rm -r tools/bwa/0.7.16a/src
+DIR=$WD/tools/bwa/0.7.16a
+URL="https://github.com/lh3/bwa/releases/download/v0.7.16/bwa-0.7.16a.tar.bz2"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/bin $DIR/share/man/man1 $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvj --strip-components=1
+    make 
+    mv bwa $DIR/bin 
+    mv *.pl $DIR/bin
+    mv bwa.1 $DIR/share/man/man1
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing samtools
-mkdir -p tools/samtools/1.7/src
-cd tools/samtools/1.7/src
-wget -qO- https://github.com/samtools/samtools/releases/download/1.7/samtools-1.7.tar.bz2 \
-    |tar xvj --strip-components=1
-./configure --prefix=$WD/tools/samtools/1.7
-make
-make install
-cd $WD
-rm -r tools/samtools/1.7/src
+DIR=$WD/tools/samtools/1.7
+URL="https://github.com/samtools/samtools/releases/download/1.7/samtools-1.7.tar.bz2"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvj --strip-components=1
+    ./configure --prefix=$DIR
+    make
+    make install
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing htslib
-mkdir -p tools/htslib/1.7/src
-cd tools/htslib/1.7/src
-wget -qO- https://github.com/samtools/htslib/releases/download/1.7/htslib-1.7.tar.bz2 \
-    |tar xvj --strip-components=1
-./configure --prefix=$WD/tools/htslib/1.7
-make
-make install
-cd $WD
-rm -r tools/htslib/1.7/src
+DIR=$WD/tools/htslib/1.7
+URL="https://github.com/samtools/htslib/releases/download/1.7/htslib-1.7.tar.bz2"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvj --strip-components=1
+    ./configure --prefix=$DIR
+    make
+    make install
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing bcftools
-mkdir -p tools/bcftools/1.7/src
-cd tools/bcftools/1.7/src
-wget -qO- https://github.com/samtools/bcftools/releases/download/1.7/bcftools-1.7.tar.bz2 \
-    |tar xvj --strip-components=1
-./configure --prefix=$WD/tools/bcftools/1.7
-make
-make install
-cd $WD
-rm -r tools/bcftools/1.7/src
+DIR=$WD/tools/bcftools/1.7
+URL="https://github.com/samtools/bcftools/releases/download/1.7/bcftools-1.7.tar.bz2"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvj --strip-components=1
+    ./configure --prefix=$DIR
+    make
+    make install
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing sambamba
-mkdir -p tools/sambamba/v0.6.7/bin
-cd tools/sambamba/v0.6.7/bin
-wget -qO- https://github.com/biod/sambamba/releases/download/v0.6.7/sambamba_v0.6.7_linux.tar.bz2 \
-    |tar xvj
-cd $WD
+DIR=$WD/tools/sambamba/0.6.7
+URL="https://github.com/biod/sambamba/releases/download/v0.6.7/sambamba_v0.6.7_linux.tar.bz2"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/bin
+    cd $DIR/bin
+    wget -qO- $URL |tar xvj
+    cd $WD
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing vt
-mkdir -p tools/vt/2018-06-07/bin
-mkdir -p tools/vt/2018-06-07/src
-cd tools/vt/2018-06-07/src
-wget -qO- http://github.com/atks/vt/archive/ee9f613.tar.gz \
-    |tar xvz --strip-components=1
-make
-mv vt ../bin
-cd $WD
-rm -r tools/vt/2018-06-07/src
+DIR=$WD/tools/vt/2018-06-07
+URL="http://github.com/atks/vt/archive/ee9f613.tar.gz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/bin $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvz --strip-components=1
+    make
+    mv vt $DIR/bin
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing root
 # prerequisite cmake > 3.4.3
-mkdir -p tools/cmake/3.11.4
-wget -qO- https://cmake.org/files/v3.11/cmake-3.11.4-Linux-x86_64.tar.gz \
-    |tar xvz --strip-components=1 -C tools/cmake/3.11.4
+DIR=$WD/tools/cmake/3.11.4
+URL="https://cmake.org/files/v3.11/cmake-3.11.4-Linux-x86_64.tar.gz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR
+    wget -qO- $URL |tar xvz --strip-components=1 -C $DIR
+    touch $DIR/installed
+fi
 
 # build root
-mkdir -p tools/root/6.14.00/src
-cd tools/root/6.14.00/src
-wget -qO- https://root.cern.ch/download/root_v6.14.00.source.tar.gz \
-    |tar xvz
-$WD/tools/cmake/3.11.4/bin/cmake \
-    -Dbuiltin_pcre=ON -Dbuiltin_vdt=ON -Dhttp=ON -Dgnuinstall=ON \
-    -DCMAKE_INSTALL_PREFIX=$WD/tools/root/6.14.00 \
-    root-6.14.00
-$WD/tools/cmake/3.11.4/bin/cmake --build . -- -j$(nproc)
-$WD/tools/cmake/3.11.4/bin/cmake --build . --target install
-cd $WD
-rm -r tools/root/6.14.00/src
+ROOTSYS=$WD/tools/root/6.14.00
+ROOTURL="https://root.cern.ch/download/root_v6.14.00.source.tar.gz"
+if [[ ! -f $ROOTSYS/installed ]]; then
+    mkdir -p $ROOTSYS/src/root
+    cd $ROOTSYS/src
+    wget -qO- $ROOTURL |tar xvz --strip-components=1 -C root
+    $DIR/bin/cmake -Dbuiltin_pcre=ON -Dbuiltin_vdt=ON -Dhttp=ON -Dgnuinstall=ON \
+        -DCMAKE_INSTALL_PREFIX=$ROOTSYS root
+    $DIR/bin/cmake --build . -- -j$(nproc)
+    $DIR/bin/cmake --build . --target install
+    cd $WD
+    rm -r $ROOTSYS/src
+    touch $ROOTSYS/installed
+fi
+unset DIR URL ROOTURL
 
 # Installing cnvnator
-source $WD/tools/root/6.14.00/bin/thisroot.sh
-mkdir -p tools/cnvnator/0.4/bin
-mkdir -p tools/cnvnator/0.4/src/samtools
-cd tools/cnvnator/0.4/src
-wget -qO- https://github.com/abyzovlab/CNVnator/archive/v0.4.tar.gz \
-    |tar xvz --strip-components=1
-cd samtools
-wget -qO- https://github.com/samtools/samtools/releases/download/1.7/samtools-1.7.tar.bz2 \
-    |tar xvj --strip-components=1
-make
-cd ..
-sed -i '2s/$/ -lpthread/' Makefile
-make OMP=no
-mv cnvnator ../bin
-mv cnvnator2VCF.pl ../bin
-mv *.py ../bin
-cd $WD
-rm -r tools/cnvnator/0.4/src
+DIR=$WD/tools/cnvnator/0.4
+URL="https://github.com/abyzovlab/CNVnator/archive/v0.4.tar.gz"
+SAMURL="https://github.com/samtools/samtools/releases/download/1.7/samtools-1.7.tar.bz2"
+set +o nounset
+source $ROOTSYS/bin/thisroot.sh
+set -o nounset
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/bin $DIR/src/samtools
+    cd $DIR/src
+    wget -qO- $URL |tar xvz --strip-components=1
+    cd samtools
+    wget -qO- $SAMURL |tar xvj --strip-components=1
+    make
+    cd ..
+    sed -i '2s/$/ -lpthread/' Makefile
+    make OMP=no
+    mv cnvnator ../bin
+    mv cnvnator2VCF.pl ../bin
+    mv *.py ../bin
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL SAMURL ROOTSYS
 
 # Installing Picard
-mkdir -p tools/picard/2.12.1
-cd tools/picard/2.12.1
-wget -q https://github.com/broadinstitute/picard/releases/download/2.12.1/picard.jar
-cd $WD
+DIR=$WD/tools/picard/2.12.1
+URL="https://github.com/broadinstitute/picard/releases/download/2.12.1/picard.jar"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR
+    cd $DIR
+    wget -q $URL
+    cd $WD
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing GATK3
-mkdir -p tools/gatk/3.7-0
-cd tools/gatk/3.7-0
-url='https://software.broadinstitute.org/gatk/download/auth?package=GATK-archive&version=3.7-0-gcfedb67'
-wget -O GenomeAnalysisTK-3.7-0-gcfedb67.tar.bz2 "$url"
-tar xjf GenomeAnalysisTK-3.7-0-gcfedb67.tar.bz2 && rm GenomeAnalysisTK-3.7-0-gcfedb67.tar.bz2
-cd $WD
+DIR=$WD/tools/gatk/3.7-0
+URL="https://software.broadinstitute.org/gatk/download/auth?package=GATK-archive&version=3.7-0-gcfedb67"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR
+    cd $DIR
+    wget -qO- $URL |tar xvj
+    cd $WD
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing GATK4
-mkdir -p tools/gatk/
-cd tools/gatk/
-url='https://github.com/broadinstitute/gatk/releases/download/4.1.2.0/gatk-4.1.2.0.zip'
-wget -O gatk-4.1.2.0.zip "$url"
-unzip gatk-4.1.2.0.zip && rm gatk-4.1.2.0.zip
-mv gatk-4.1.2.0 4.1-2
-cd $WD
+DIR=$WD/tools/gatk/4.1-2
+URL="https://github.com/broadinstitute/gatk/releases/download/4.1.2.0/gatk-4.1.2.0.zip"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget $URL
+    unzip gatk-4.*.zip
+    rm gatk-4.*.zip
+    mv gatk-4.*/* $DIR
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
 # Installing R
-mkdir -p tools/r/3.6.1/src
-cd tools/r/3.6.1/src
-wget -qO- https://cran.r-project.org/src/base/R-3/R-3.6.1.tar.gz \
-    |tar xvz --strip-components=1
-./configure --prefix=$WD/tools/r/3.6.1
-make
-make install
-cd $WD
-rm -r tools/r/3.6.1/src
+DIR=$WD/tools/r/3.6.1
+URL="https://cran.r-project.org/src/base/R-3/R-3.6.1.tar.gz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvz --strip-components=1
+    ./configure --prefix=$DIR
+    make
+    make install
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+
+# Insatlling R packages needed
+$DIR/bin/R --no-save <<'RCODE'
+# for MosaicForecast
+install.packages(setdiff(
+    c("XML", "ggplot2", "caret", "e1071", "glmnet", "RColorBrewer", "devtools", "nnet"),
+    installed.packages()[,"Package"]), repos="https://cloud.r-project.org")
+
+# for RestroSom
+install.packages(setdiff(
+    c("randomForest", "glmnet", "e1071", "PRROC"), 
+    installed.packages()[,"Package"]), repos="https://cloud.r-project.org")
+RCODE
+unset DIR URL
 
 # Installing MosaicForecast and depending tools
 cd tools
-git clone https://github.com/parklab/MosaicForecast.git MosaicForecast
+[[ ! -d MosaicForecast ]] && git clone https://github.com/parklab/MosaicForecast.git MosaicForecast
 cd $WD
 
 mkdir -p tools/ucsc
 cd tools/ucsc
-wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/wigToBigWig
-wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/bigWigAverageOverBed
-wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/fetchChromSizes
-chmod +x wigToBigWig bigWigAverageOverBed fetchChromSizes
+for CMD in wigToBigWig bigWigAverageOverBed fetchChromSizes; do
+    if [[ ! -x $CMD ]]; then
+        wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/$CMD
+        chmod +x $CMD
+    fi
+done
 cd $WD
 
-tools/python/3.6.2/bin/pip install pysam numpy pandas pysamstats regex scipy pyfaidx 
+# Installing Perl and modules needed
+DIR=$WD/tools/perl/5.28.1
+URL="https://www.cpan.org/src/5.0/perl-5.28.1.tar.gz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvz --strip-components=1
+    ./Configure -des -Dprefix=$DIR
+    make
+    make test
+    make install
+    cd $WD
+    rm -rf $DIR/src
+    touch $DIR/installed
+fi
 
-tools/r/3.6.1/bin/R --no-save <<'RCODE'
-install.packages(setdiff(
-    c("XML", "ggplot2", "caret", "e1071", "glmnet", "RColorBrewer", "devtools", "nnet"),
-    installed.packages()[,"Package"]),
-    repos="https://cloud.r-project.org")
-RCODE
-
-# Installing Perl
-mkdir -p tools/perl/5.28.1/src
-cd tools/perl/5.28.1/src
-wget -qO- https://www.cpan.org/src/5.0/perl-5.28.1.tar.gz \
-    |tar xvz --strip-components=1
-./Configure -des -Dprefix=$WD/tools/perl/5.28.1
-make
-make test
-make install
-cd $WD
-rm -rf tools/perl/5.28.1/src
+# Installing Perl modules needed
+for MD in GD GD::Arrow GD::SVG Parallel::ForkManager; do
+    wget -qO- https://cpanmin.us |$DIR/bin/perl - $MD
+done
+unset DIR URL
 
 # Installing RetroSom and depending tools
-wget -qO- https://cpanmin.us | tools/perl/5.28.1/bin/perl - GD
-wget -qO- https://cpanmin.us | tools/perl/5.28.1/bin/perl - GD::Arrow
-wget -qO- https://cpanmin.us | tools/perl/5.28.1/bin/perl - GD::SVG
-wget -qO- https://cpanmin.us | tools/perl/5.28.1/bin/perl - Parallel::ForkManager
+DIR=$WD/tools/bedtools/2.28.0
+URL="https://github.com/arq5x/bedtools2/releases/download/v2.28.0/bedtools-2.28.0.tar.gz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvz --strip-components=1
+    make
+    mv bin ..
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
-tools/r/3.6.1/bin/R --no-save <<'RCODE'
-install.packages(setdiff(
-    c("randomForest", "glmnet", "e1071", "PRROC"), 
-    installed.packages()[,"Package"]), 
-    repos="https://cloud.r-project.org")
-RCODE
+DIR=$WD/tools/exonerate/2.2.0
+URL="http://ftp.ebi.ac.uk/pub/software/vertebrategenomics/exonerate/exonerate-2.2.0.tar.gz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget -qO- $URL |tar xvz --strip-components=1
+    ./configure --prefix=$DIR
+    make
+    make install
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
 
-mkdir -p tools/bedtools/2.28.0/src
-cd tools/bedtools/2.28.0/src
-wget -qO- https://github.com/arq5x/bedtools2/releases/download/v2.28.0/bedtools-2.28.0.tar.gz \
-    |tar xvz --strip-components=1
-make
-mv bin ..
-cd $WD
-rm -r tools/bedtools/2.28.0/src
+DIR=$WD/tools/bwakit/0.7.15
+URL="https://sourceforge.net/projects/bio-bwa/files/bwakit/bwakit-0.7.15_x64-linux.tar.bz2/download"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR
+    cd $DIR
+    wget -qO- $URL |tar xvj --strip-components=1
+    cd $WD
+    touch $DIR/installed
+fi
+unset DIR URL
 
-mkdir -p tools/exonerate/2.2.0/src
-cd tools/exonerate/2.2.0/src
-wget -qO- http://ftp.ebi.ac.uk/pub/software/vertebrategenomics/exonerate/exonerate-2.2.0.tar.gz \
-    |tar xvz --strip-components=1
-./configure --prefix=$WD/tools/exonerate/2.2.0
-make
-make install
-cd $WD
-rm -r tools/exonerate/2.2.0/src
+DIR=$WD/tools/fastqc/0.11.8
+URL="http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.8.zip"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR/src
+    cd $DIR/src
+    wget $URL
+    unzip fastqc*.zip
+    mv FastQC/* $DIR
+    cd $WD
+    rm -r $DIR/src
+    touch $DIR/installed
+fi
+unset DIR URL
+
+DIR=$WD/tools/blast/2.9.0
+URL="ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.9.0/ncbi-blast-2.9.0+-x64-linux.tar.gz"
+if [[ ! -f $DIR/installed ]]; then
+    mkdir -p $DIR
+    cd $DIR
+    wget -qO- $URL |tar xvz --strip-components=1
+    cd $WD
+    touch $DIR/installed
+fi
+unset DIR URL
