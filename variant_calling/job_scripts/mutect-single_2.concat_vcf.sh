d62ceb25a3bbba6eba9be21ded1f7e9507bce682
Taejeong Bae <bintriz@gmail.com>
2019-08-14 11:28:54 -0500

    Add mutect-sinle job scripts

---
+++ variant_calling/job_scripts/mutect-single_2.concat_vcf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe threaded 3
+
+trap "exit 100" ERR  
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $(basename $0) <sample name>"
+    exit 1
+fi
+
+SM=$1
+
+source $(pwd)/$SM/run_info
+
+set -o nounset
+set -o pipefail
+
+CHR_VCFS=""
+for CHR in $(seq 1 22) X Y; do
+    CHR_VCFS="$CHR_VCFS -I $SM/mutect2-single/$SM.$CHR.vcf"
+done
+VCF=$SM/mutect2-single/$SM.vcf
+
+printf -- "---\n[$(date)] Start concat vcfs.\n"
+
+if [[ ! -f $VCF.idx ]]; then
+    $GATK4 --java-options "-Xmx4G"  GatherVcfs \
+        -R $REF \
+        $CHR_VCFS \
+        -O $VCF \
+
+    for CHR in $(seq 1 22) X Y; do
+        rm $SM/mutect2-single/$SM.$CHR.vcf
+        rm $SM/mutect2-single/$SM.$CHR.vcf.idx
+    done
+else
+    echo "Skip this step."
+fi
+
+printf -- "[$(date)] Finish concat vcfs.\n---\n"
