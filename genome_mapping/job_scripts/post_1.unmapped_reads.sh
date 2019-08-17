e31080edd1cc9cccebafd4e87e0ba4f367c7a929
Taejeong Bae <bintriz@gmail.com>
2019-08-17 08:57:31 -0500

    Change the final file format of output from bam to cram

---
+++ genome_mapping/job_scripts/post_1.unmapped_reads.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe threaded 8
+
+trap "exit 100" ERR
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $(basename $0) <sample name>"
+    false
+fi
+
+SM=$1
+
+source $(pwd)/$SM/run_info
+
+set -o nounset
+set -o pipefail
+
+CRAM=$SM/alignment/$SM.cram
+UNMAPPED=$SM/alignment/$SM.unmapped.bam
+
+printf -- "---\n[$(date)] Start extracting unmapped reads: $BAM\n"
+
+# An unmapped read whose mate is mapped
+$SAMTOOLS view -@ $NSLOTS -T $REF -f 4 -F 264 -u -o $SM/alignment/tmps1.bam $CRAM
+# A mapped read who’s mate is unmapped
+$SAMTOOLS view -@ $NSLOTS -T $REF -f 8 -F 260 -u -o $SM/alignment/tmps2.bam $CRAM
+# Both reads of the pair are unmapped
+$SAMTOOLS view -@ $NSLOTS -T $REF -f 12 -F 256 -u -o $SM/alignment/tmps3.bam $CRAM
+    
+$SAMTOOLS merge -u - $SM/alignment/tmps[123].bam \
+    |$SAMTOOLS sort -@ $((NSLOTS-1)) -m 3.8G -n -o $UNMAPPED -
+
+rm $SM/alignment/tmps[123].bam
+
+printf -- "[$(date)] Finish extracting unmapped reads: $BAM\n---\n"
