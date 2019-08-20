226e69e7d4ad05d4c7e15541a88a0412a528bc00
Taejeong Bae <bintriz@gmail.com>
2019-08-20 02:51:50 -0500

    Update wrapper scripts

--- variant_calling.sh
+++ variant_calling.sh
@@ -3,4 +3,7 @@
 PIPE_HOME=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 PYTHON3=$PIPE_HOME/$(grep PYTHON3 $PIPE_HOME/config.ini |cut -f2 -d=|sed 's/^ \+//')
 
-$PYTHON3 $PIPE_HOME/variant_calling/run.py $@
+cmd=$(basename $0)
+$PYTHON3 $PIPE_HOME/variant_calling/run.py $@ \
+    1> >(sed "s/run.py/$cmd/" >&1) \
+    2> >(sed "s/run.py/$cmd/" >&2)
