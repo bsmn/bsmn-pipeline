11c9809265ffc31f7840dbbe185677c1d6cd2d13
Taejeong Bae <bintriz@gmail.com>
2019-08-25 02:27:02 -0500

    Rearrange job orders

--- jobs/submit_aln_jobs.py
+++ jobs/submit_aln_jobs.py
@@ -16,7 +16,7 @@ from library.job_queue import GridEngineQueue
 def main():
     args = parse_args()
     q = GridEngineQueue()
-    ''' 
+
     jid_list = []
     for pu in [fastq.split(".")[1] for fastq in glob.glob("{sample}/fastq/{sample}.*.R1.fastq.gz".format(sample=args.sample_name))]:
         jid_list.append(q.submit(opt(args.sample_name), 
@@ -34,18 +34,16 @@ def main():
     jid = q.submit(opt(args.sample_name, jid), 
         "{job_home}/aln_5.bqsr.sh {sample}".format(job_home=job_home, sample=args.sample_name))
 
-'''
-    jid = None
     jid = q.submit(opt(args.sample_name, jid), 
         "{job_home}/post_1.unmapped_reads.sh {sample}".format(job_home=job_home, sample=args.sample_name))
 
     save_hold_jid("{sample}/alignment/hold_jid".format(sample=args.sample_name), jid)
 
-    q.submit(opt(args.sample_name), 
-        "{job_home}/post_2.upload_cram.sh {sample}".format(job_home=job_home, sample=args.sample_name))
+    jid = q.submit(opt(args.sample_name), 
+        "{job_home}/post_2.run_variant_calling.sh {sample}".format(job_home=job_home, sample=args.sample_name))
 
-    q.submit(opt(args.sample_name), 
-        "{job_home}/post_3.run_variant_calling.sh {sample}".format(job_home=job_home, sample=args.sample_name))
+    q.submit(opt(args.sample_name, jid), 
+        "{job_home}/post_3.upload_cram.sh {sample}".format(job_home=job_home, sample=args.sample_name))
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Alignment job submitter')
