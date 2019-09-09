2ab14a69fcf586e078f8179796dfcd08d39825f7
Taejeong Bae <bintriz@gmail.com>
2019-09-09 16:11:40 -0500

    Bug fix: run ssh excute remote commands as a login shell

--- jobs/genome_mapping/post_2.run_variant_calling.sh
+++ jobs/genome_mapping/post_2.run_variant_calling.sh
@@ -25,16 +25,16 @@ if [[ $RUN_GATK_HC = "False" ]] \
 else
     if [[ $RUN_GATK_HC = "True" ]]; then
         ssh -o StrictHostKeyChecking=no $SGE_O_HOST \
-            "cd $SGE_O_WORKDIR; $PYTHON3 $PIPE_HOME/jobs/submit_gatk-hc_jobs.py --ploidy $PLOIDY --sample-name $SM"
+            "bash --login -c 'cd $SGE_O_WORKDIR; $PYTHON3 $PIPE_HOME/jobs/submit_gatk-hc_jobs.py --ploidy $PLOIDY --sample-name $SM'"
         echo "Submitted gatk-hc jobs."
     fi
     if [[ $RUN_MUTECT_SINGLE = "True" ]]; then
         ssh -o StrictHostKeyChecking=no $SGE_O_HOST \
-            "cd $SGE_O_WORKDIR; $PYTHON3 $PIPE_HOME/jobs/submit_mutect-single_jobs.py --sample-name $SM"
+            "bash --login -c 'cd $SGE_O_WORKDIR; $PYTHON3 $PIPE_HOME/jobs/submit_mutect-single_jobs.py --sample-name $SM'"
     fi
     if [[ $RUN_CNVNATOR = "True" ]]; then
         ssh -o StrictHostKeyChecking=no $SGE_O_HOST \
-            "cd $SGE_O_WORKDIR; $PYTHON3 $PIPE_HOME/jobs/submit_cnvnator_jobs.py --sample-name $SM"
+            "bash --login -c 'cd $SGE_O_WORKDIR; $PYTHON3 $PIPE_HOME/jobs/submit_cnvnator_jobs.py --sample-name $SM'"
         echo "Submitted cnvnator jobs."
     fi
 fi
