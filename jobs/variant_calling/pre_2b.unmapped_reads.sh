e3ec9b44e5783ad208ae695cf2d6ae206b88e68c
Taejeong Bae <bintriz@gmail.com>
2019-10-09 17:10:28 -0500

    Incorporating bam2cram conversion steps to variant calling.

--- jobs/variant_calling/pre_2b.unmapped_reads.sh
+++ jobs/variant_calling/pre_2b.unmapped_reads.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -pe threaded 16
+#$ -pe threaded 4
 
 trap "exit 100" ERR
 
@@ -16,24 +16,28 @@ source $(pwd)/$SM/run_info
 set -o nounset
 set -o pipefail
 
-BAM=$SM/alignment/$SM.bam
+DONE=$SM/run_status/pre_2b.unmapped_reads.done
+
+CRAM=$SM/alignment/$SM.cram
 UNMAPPED=$SM/alignment/$SM.unmapped.bam
 
-printf -- "---\n[$(date)] Start extracting unmapped reads: $BAM\n"
-if [[ -f $BAM ]]; then
+printf -- "---\n[$(date)] Start extracting unmapped reads: $CRAM\n"
+
+if [[ -f $DONE ]]; then
+    echo "Skip this step."
+else
     # An unmapped read whose mate is mapped
-    $SAMTOOLS view -@16 -u -f 4 -F 264 -o $SM/alignment/tmps1.bam $BAM
+    $SAMTOOLS view -@ $NSLOTS -T $REF -f 4 -F 264 -u -o $SM/alignment/tmps1.bam $CRAM
     # A mapped read who’s mate is unmapped
-    $SAMTOOLS view -@16 -u -f 8 -F 260 -o $SM/alignment/tmps2.bam $BAM
+    $SAMTOOLS view -@ $NSLOTS -T $REF -f 8 -F 260 -u -o $SM/alignment/tmps2.bam $CRAM
     # Both reads of the pair are unmapped
-    $SAMTOOLS view -@16 -u -f 12 -F 256 -o $SM/alignment/tmps3.bam $BAM
+    $SAMTOOLS view -@ $NSLOTS -T $REF -f 12 -F 256 -u -o $SM/alignment/tmps3.bam $CRAM
     
     $SAMTOOLS merge -u - $SM/alignment/tmps[123].bam \
-        |$SAMTOOLS sort -@15 -m 3.8G -n -o $UNMAPPED -
-    $SAMTOOLS index $UNMAPPED
+        |$SAMTOOLS sort -@ $((NSLOTS-1)) -m 3700M -n -o $UNMAPPED -
 
     rm $SM/alignment/tmps[123].bam
-else
-    echo "Skip this step."
+    touch $DONE
 fi
-printf -- "[$(date)] Finish extracting unmapped reads: $BAM\n---\n"
+
+printf -- "[$(date)] Finish extracting unmapped reads: $CRAM\n---\n"
