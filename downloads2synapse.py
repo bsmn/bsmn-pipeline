3dffa9d4637b58f42ff25f953a9aab096d04f293
Attila Jones <attila.g.jones@gmail.com>
2020-03-17 18:12:26 -0400

    downloads to synapse script documented

--- downloads2synapse.py
+++ downloads2synapse.py
@@ -8,6 +8,17 @@ import synapseutils
 import argparse
 
 def files2synapse(subd, maind, downloadsFolder, syn):
+    '''
+    Upload all files in subdirectory to Synapse
+
+    Parameters
+    subd: (basename of) subdirectory
+    maind: path to main directory
+    downloadsFolder: the object of "bsmn-pipeline-downloads" Synapse folder
+    syn: a synapseclient.Synapse object with logged in state
+
+    Value: subd and list of file paths
+    '''
     # create or update synapse folder named subd
     folder = synapseclient.Folder(subd, parent=downloadsFolder)
     folder = syn.store(folder)
@@ -23,6 +34,21 @@ def files2synapse(subd, maind, downloadsFolder, syn):
     return(subd, flist)
 
 def main(maind, mainFolderID, syn):
+    '''
+    Upload subdirectories of main dir together with their contents to Synapse
+
+    Parameters
+    maind: path to main directory
+    mainFolderID: the ID of the Synapse folder or project to contain the "bsmn-pipeline-downloads" folder
+    syn: a synapseclient.Synapse object with logged in state
+
+    Value: the list of file lists; each file list corresponds to a subdirectory under maind
+
+    Details
+    The source is all the files in all subdirectories under "maind".  The
+    target is the "bsmn-pipeline-downloads" Synapse folder to be created or
+    updated under "mainFolderID"
+    '''
     # create or update synapse folder named subd
     downloadsFolder = synapseclient.Folder('bsmn-pipeline-downloads', parent=mainFolderID)
     downloadsFolder = syn.store(downloadsFolder)
