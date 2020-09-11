786f0dd35a2201f3fa85ad721fcdd2b1a192ce30
Yeongjun Jang <yjjang2050@gmail.com>
2020-09-10 23:07:51 -0500

    Remove old wrapped executables

--- run_genome_mapping.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PIPE_HOME=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
-PYTHON3=$PIPE_HOME/$(grep PYTHON3 $PIPE_HOME/config.ini |cut -f2 -d=|sed 's/^ \+//')
-
-cmd=$(basename $0)
-$PYTHON3 $PIPE_HOME/jobs/run_genome_mapping.py $@ \
-    1> >(sed "s/run_genome_mapping.py/$cmd/" >&1) \
-    2> >(sed "s/run_genome_mapping.py/$cmd/" >&2)
