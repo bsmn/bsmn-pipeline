30148c3cf4d387d79349181524ad22157f186b83
yjjang <yjjang2050@gmail.com>
2021-10-29 22:23:48 -0500

    Fix a url for downloading hg38 reference

--- download_resources.sh
+++ download_resources.sh
@@ -40,24 +40,24 @@ fi
 # Download and index the humnan reference genome (hg38)
 # You already have these files? Just copy or link them into $RESDIR/hg38 to skip to the next step.
 if [[ ! -f $RESDIR/hg38/dbsnp_146.hg38.vcf.gz.tbi ]]; then
-    gsutil -m cp gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.fai \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.amb \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.ann \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.bwt \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.pac \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.sa \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.dict \
-                 gs://gcp-public-data--broad-references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
-                 gs://gcp-public-data--broad-references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi \
-                 gs://gcp-public-data--broad-references/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz \
-                 gs://gcp-public-data--broad-references/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz.tbi \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz \
-                 gs://gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz.tbi \
-                 gs://gcp-public-data--broad-references/hg38/v0/1000G_omni2.5.hg38.vcf.gz \
-                 gs://gcp-public-data--broad-references/hg38/v0/1000G_omni2.5.hg38.vcf.gz.tbi \
-                 gs://gcp-public-data--broad-references/hg38/v0/hapmap_3.3.hg38.vcf.gz \
-                 gs://gcp-public-data--broad-references/hg38/v0/hapmap_3.3.hg38.vcf.gz.tbi \
+    gsutil -m cp gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta.fai \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta.amb \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta.ann \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta.bwt \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta.pac \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta.sa \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.dict \
+                 gs://genomics-public-data/references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
+                 gs://genomics-public-data/references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi \
+                 gs://genomics-public-data/references/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz \
+                 gs://genomics-public-data/references/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz.tbi \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz \
+                 gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz.tbi \
+                 gs://genomics-public-data/references/hg38/v0/1000G_omni2.5.hg38.vcf.gz \
+                 gs://genomics-public-data/references/hg38/v0/1000G_omni2.5.hg38.vcf.gz.tbi \
+                 gs://genomics-public-data/references/hg38/v0/hapmap_3.3.hg38.vcf.gz \
+                 gs://genomics-public-data/references/hg38/v0/hapmap_3.3.hg38.vcf.gz.tbi \
                  $RESDIR/hg38
     cd $RESDIR/hg38
     lftp -c 'set ftp:web-mode true; pget -n 10 -c ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz'
